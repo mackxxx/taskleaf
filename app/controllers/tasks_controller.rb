b42493b9af2b277dace99008c3feb96cfd33b8e8
mackxxx <42348939+mackxxx@users.noreply.github.com>
2021-01-30 18:54:01 +0900

    CSV-エクスポート

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,58 +1,64 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @q = current_user.tasks.ransack(params[:q])
     @tasks = @q.result(distinct: true)
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @tasks.generate_csv, filename: "tasks-#{Time.zone.now.strftime('%Y%m%d%S')}.csv" } # --- ②
+    end
   end
 
   def show
   end
 
   def new
-    @task = Task.new 
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
   end
 
   def create
     @task = current_user.tasks.new(task_params)
 
     if params[:back].present?
-       render :new
-       return
+      render :new
+      return
     end
 
     if @task.save
       TaskMailer.creation_email(@task).deliver_now
-      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました！"
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
     end
   end
 
-  def edit
-  end
-
   def update
     @task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました！"
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
   end
 
   def destroy
     @task.destroy
-    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました！"
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
-  def confirm_new
-    @task = current_user.tasks.new(task_params)
-    render :new unless @task.valid?     
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :image)
   end
 
-  private     
-    def task_params
-      params.require(:task).permit(:name, :description, :image)
-    end
-
-    def set_task
-      @task = current_user.tasks.find(params[:id])
-    end
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,28 +1,29 @@
 class Task < ApplicationRecord
   has_one_attached :image
 
-  def self.ransackable_attributes(auth_object = nil)
-    %w[name created_at]
-  end
-
-  def self.ransacklable_associations(auth_object = nil)
-    []    
-  end
-  
-  before_validation :set_nameless_name
-  validates :name, presence: true, length: {maximum: 30}
-  validate :validate_name_not_including_commma
+  validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
 
   belongs_to :user
-  scope :recent, -> {order(created_at: :desc)}
+
+  scope :recent, -> { order(created_at: :desc) }
+
+  def self.csv_attributes
+    ["name", "description", "created_at", "updated_at"]
+  end
+
+  def self.generate_csv
+    CSV.generate(headers: true) do |csv|
+      csv << csv_attributes
+      all.each do |task|
+        csv << csv_attributes.map{ |attr| task.send(attr) }
+      end
+    end
+  end
 
   private
-  
-  def set_nameless_name
-    self.name = '名前なし' if name.blank?
-  end
-  
-  def validate_name_not_including_commma
+
+  def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
   end
 end
